e88003b9abdbd11e7047ad450d35ad6d6a3d14c4
creasetoph <chris.albert@ticketfly.com>
2015-10-15 17:21:58 -0700

    Adding browserfy

---
+++ js/browserfy-app.js
@@ -0,0 +1,10 @@
+var DomReady   = require('domready');
+var _          = require('jquery');
+var $          = require('underscore');
+var Handlebars = require('handlebars');
+var Backbone   = require('backbone');
+var Bluebird   = require('Bluebird');
+
+DomReady(function() {
+  console.log('im loaded in dom ready');
+});
