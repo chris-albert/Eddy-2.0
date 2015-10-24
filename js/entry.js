d078e09ae33be0ecbb7f88b3e2810f2a71e98c46
creasetoph <chris.albert@ticketfly.com>
2015-10-23 17:33:48 -0700

    Spliting views

--- js/entry.js
+++ js/entry.js
@@ -1,6 +1,13 @@
-var DomReady = require('domready');
-var Swagger  = require('./swagger');
+var DomReady  = require('domready');
+var Swagger   = require('./swagger');
+var _         = require('underscore');
+
+function bindSemanticJquery() {
+  window.jQuery = require('jquery');
+  require('../semantic/dist/components/accordion');
+}
 
 DomReady(function() {
+  bindSemanticJquery();
   new Swagger.ApiInfo();
 });
